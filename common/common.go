37fd9dc42932c89bcd132854398d462923ef9264
WGCAT <seed9878@naver.com>
2024-06-17 15:05:53 +0900

    (feat): BATCH gRPC호출 임시로직 추가

---
+++ common/common.go
@@ -0,0 +1,13 @@
+package common
+
+import "main/model"
+
+// 사건 저장 grpc (임시)
+func SetEvents(atHistory []model.AtHistory) (mapData string, err error) {
+	return
+}
+
+// 사건 위치 저장 grpc (임시)
+func SetHistories(mapData string) (err error) {
+	return
+}
